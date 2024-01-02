3f50d644012a2f0cfc95dae73e0f16c43625a896
Sadid <76834719+Sadid-USN@users.noreply.github.com>
2024-01-02 20:22:20 +0600

    fixed gradle

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
   await DBHelper.initDB();
-  await NotificationHelper().initNotification();
+ // await NotificationHelper().initNotification();
   await GetStorage.init();
 
   runApp(MultiProvider(
